42abe5f8305a01bf29a077502f2dad37a999094e
Daniel-Timmy <ajayitimmy45@gmail.com>
2025-09-26 21:32:22 +0100

    hhhh

--- BaselinksWebsite/src/components/process/Steps.jsx
+++ BaselinksWebsite/src/components/process/Steps.jsx
@@ -1,13 +1,89 @@
 import React from "react";
 import { STEP1, STEP2, STEP3, STEP4 } from "../../constants";
 
-const Steps = () => {
-  return (
-    <div className="px-4 mt-40 w-screen flex">
-      <div></div>
+import { useState, useEffect, useRef } from "react";
+import "./Stepbar.css";
 
-      <div className="flex flex-col text-left w-screen sm:space-y-15 space-y-10">
-        <div className="space-y-6 overflow-hidden">
+const Stepbar = ({ setCurrentStep, currentStep }) => {
+  const steps = [
+    { label: "discover" },
+    { label: "strategy" },
+    { label: "design" },
+    { label: "deliver" },
+  ];
+
+  return (
+    <div className="progress-container">
+      <div className="progress-bar">
+        {steps.map((step, index) => (
+          <div key={index} className="step-container">
+            <div className={`step ${index <= currentStep ? "active" : ""}`}>
+              <div className="step-number">
+                {index < currentStep ? "✔" : index + 1}
+              </div>
+              <div className="label"></div>
+            </div>
+            {index < steps.length - 1 && (
+              <div
+                className={`connector h-40 sm:h-100 lg:h-170 ${
+                  index < currentStep ? "active" : ""
+                }`}
+              ></div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Steps = () => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const stepRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.3, // Trigger when 50% of the section is visible
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const index = stepRefs.findIndex(
+            (ref) => ref.current === entry.target
+          );
+          if (index !== -1) {
+            setCurrentStep(index);
+          }
+        }
+      });
+    }, options);
+
+    stepRefs.forEach((ref) => {
+      if (ref.current) {
+        observer.observe(ref.current);
+      }
+    });
+
+    return () => {
+      stepRefs.forEach((ref) => {
+        if (ref.current) {
+          observer.unobserve(ref.current);
+        }
+      });
+    };
+  }, [stepRefs]);
+
+  return (
+    <div className="px-4 lg:px-6 mt-40 w-screen flex justify-between ">
+      <div className=" h-screen w-1/4 lg:w-1/3 ">
+        <Stepbar setCurrentStep={setCurrentStep} currentStep={currentStep} />
+      </div>
+
+      <div className="flex flex-col text-left sm:space-y-15 space-y-10">
+        <div ref={stepRefs[0]} className="lg:space-y-6 overflow-hidden">
           <h3 className="text-xl/15 sm:text-3xl/15 lg:text-5xl/20 px-4 font-trajan text-hero-text">
             Discover
           </h3>{" "}
@@ -15,7 +91,7 @@ const Steps = () => {
             style={{
               backgroundImage: `url(${STEP1})`,
             }}
-            className="relative group flex flex-col justify-end bg-cover bg-no-repeat rounded-xl sm:rounded-[32px] w-full h-33.5 sm:w-160.5 sm:h-76.5 lg:w-281 lg:h-134.5 p-4"
+            className="relative group flex flex-col max-sm:items-center max-sm:justify-center sm:justify-end bg-cover bg-no-repeat rounded-xl sm:rounded-[32px] w-full h-33.5 sm:w-160.5 sm:h-76.5 lg:w-281 lg:h-134.5 p-4"
           >
             <div className="absolute inset-0 h-full bg-[#EBEBD6] opacity-0  lg:opacity-20 lg:group-hover:opacity-0 transition-opacity duration-300 z-0"></div>
 
@@ -29,7 +105,7 @@ const Steps = () => {
             </div>
           </div>
         </div>
-        <div className="space-y-6 relative overflow-hidden">
+        <div ref={stepRefs[1]} className="space-y-6 relative overflow-hidden">
           <h3 className="text-xl/15 sm:text-3xl/15 lg:text-5xl/20 px-4 font-trajan text-hero-text">
             Strategy
           </h3>{" "}
@@ -37,7 +113,7 @@ const Steps = () => {
             style={{
               backgroundImage: `url(${STEP2})`,
             }}
-            className="group flex flex-col justify-end bg-cover bg-no-repeat rounded-xl sm:rounded-[32px] w-full h-33.5 sm:w-160.5 sm:h-76.5 lg:w-281 lg:h-134.5 p-4"
+            className="group flex flex-col max-sm:items-center max-sm:justify-center sm:justify-end bg-cover bg-no-repeat rounded-xl sm:rounded-[32px] w-full h-33.5 sm:w-160.5 sm:h-76.5 lg:w-281 lg:h-134.5 p-4"
           >
             <div className="absolute inset-0 h-full bg-[#EBEBD6] opacity-0  lg:opacity-20 lg:group-hover:opacity-0 transition-opacity duration-300 z-0"></div>
 
@@ -50,7 +126,7 @@ const Steps = () => {
             </div>
           </div>
         </div>
-        <div className="space-y-6 relative overflow-hidden">
+        <div ref={stepRefs[2]} className="space-y-6 relative overflow-hidden">
           <h3 className="text-xl/15 sm:text-3xl/15 lg:text-5xl/20 px-4 font-trajan text-hero-text">
             Design
           </h3>{" "}
@@ -58,7 +134,7 @@ const Steps = () => {
             style={{
               backgroundImage: `url(${STEP3})`,
             }}
-            className="group flex flex-col justify-end bg-cover bg-no-repeat rounded-xl sm:rounded-[32px] w-full h-33.5 sm:w-160.5 sm:h-76.5 lg:w-281 lg:h-134.5 p-4"
+            className="group flex flex-col max-sm:items-center max-sm:justify-center sm:justify-end bg-cover bg-no-repeat rounded-xl sm:rounded-[32px] w-full h-33.5 sm:w-160.5 sm:h-76.5 lg:w-281 lg:h-134.5 p-4"
           >
             <div className="absolute inset-0 h-full bg-[#EBEBD6] opacity-0  lg:opacity-20 lg:group-hover:opacity-0 transition-opacity duration-300 z-0  lg:pointer-events-none lg:group-hover:pointer-events-auto"></div>
 
@@ -71,7 +147,7 @@ const Steps = () => {
             </div>
           </div>
         </div>
-        <div className="space-y-6 relative overflow-hidden">
+        <div ref={stepRefs[4]} className="space-y-6 relative overflow-hidden">
           <h3 className="text-xl/15 sm:text-3xl/15 lg:text-5xl/20 px-4 font-trajan text-hero-text">
             Deliver
           </h3>{" "}
@@ -79,7 +155,7 @@ const Steps = () => {
             style={{
               backgroundImage: `url(${STEP4})`,
             }}
-            className=" group flex flex-col justify-end bg-cover bg-no-repeat rounded-xl sm:rounded-[32px] w-full h-33.5 sm:w-160.5 sm:h-76.5 lg:w-281 lg:h-134.5 p-4"
+            className=" group flex flex-col max-sm:items-center max-sm:justify-center sm:justify-end bg-cover bg-no-repeat rounded-xl sm:rounded-[32px] w-full h-33.5 sm:w-160.5 sm:h-76.5 lg:w-281 lg:h-134.5 p-4"
           >
             <div className="absolute inset-0 h-full bg-[#EBEBD6] opacity-0  lg:opacity-20 lg:group-hover:opacity-0 transition-opacity duration-300 z-0"></div>
 
