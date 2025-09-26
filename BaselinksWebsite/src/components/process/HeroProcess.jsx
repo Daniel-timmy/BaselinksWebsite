2e241a1b293f9a0a7a00722f46e613f9e6637e4b
Daniel-Timmy <ajayitimmy45@gmail.com>
2025-09-26 17:18:11 +0100

    fgh

--- BaselinksWebsite/src/components/process/HeroProcess.jsx
+++ BaselinksWebsite/src/components/process/HeroProcess.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="mt-36 px-4 space-y-6 w-screen">
+    <div className="mt-36 px-4 space-y-6 w-screen flex flex-col items-center justify-center">
       <h5 className="font-trajan text-[32px]/10 sm:text-[40px]/15 lg:text-[60px]/20 text-center text-black sm:px-10">
         How We Make It
         <span className="text-baselinks-yellow"> Happen</span>
       </h5>
-      <p className="text-[16px]/5 text-hero-text lg:text-[20px]/6 sm:w-161">
+      <p className="text-[16px]/5 text-hero-text lg:text-[20px]/6 sm:w-161 text-center">
         Since 2008, our team has refined a four-step approach, blending creative
         mastery with technical precision to deliver standout results with
         tailored solutions.
